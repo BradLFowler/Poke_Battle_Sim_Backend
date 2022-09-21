efcbead26c36ab322b74ec58a4ba30f21631ea02
BradLFowler <BradLF00@gmail.com>
2022-09-21 18:53:49 -0500

    fixing cors issue

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require('express')
 const authRouter = require('./routes/auth')
 const usersRouter = require('./routes/users')
 const { logger } = require('./middleware/middleware')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express();
 app.use(express.json())
 const port = process.env.PORT;
+
+const corsOptions = {
+    origin: 'http://localhost:3000', 
+    credentials: true,//access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
+app.use(cors)
 
 app.get('/', logger, (req, res) => {
     res.send('Welcome')
